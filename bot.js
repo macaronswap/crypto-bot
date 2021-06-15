6f92f5582c3a28a794afccc12ea698b984a449f8
chocofalls <chocofallsdev@gmail.com>
2021-06-15 23:04:21 +0300

    cleaning, gecko api, macaron api

--- bot.js
+++ bot.js
@@ -1,280 +1,132 @@
-require('dotenv').config();
-const TeleBot = require('telebot');
-const Binance = require('node-binance-api');
-const CoinMarketCap = require('coinmarketcap-api');
-const request = require('request');
+require("dotenv").config();
+const TeleBot = require("telebot");
+const request = require("request");
+const CoinGecko = require("coingecko-api");
 
 const bot = new TeleBot(process.env.TELEGRAM_TOKEN);
-const binance = new Binance({
-  APIKEY: process.env.BINANCE_KEY,
-  APISECRET: process.env.BINANCE_SECRET,
-});
-
-const cmcClient = new CoinMarketCap(process.env.COINMARKETCAP_KEY);
+const CoinGeckoClient = new CoinGecko();
 
 const TOO_MUCH = "You're using the bot too much!";
-const NO_CURRENCY = 'No currency found with that name.';
-const NO_TICKER = 'Ticker not found.';
-const RANK_NOT_IN_RANGE = 'Given rank must be greater than 1.';
-const NO_CURRENCY_RANK = 'No currency found with that rank.';
-const DEPRECATED = 'Deprecated';
 
 let cache = { global: {}, bin: {} };
 
-bot.on('/start', (msg) => {
+bot.on("/start", (msg) => {
   msg.reply.text(
-    '/price for current price of Macaron (MCRN)\n' +
-    '/priceCMC for current Coinmarketcap price of Macaron (MCRN)\n' +
-    '/info for information Macaron (MCRN)\n'
+    "/price for current price of Macaron (MCRN)\n" +
+      "/info for information Macaron (MCRN)\n",
   );
 });
 
-// Ticker information from CoinMarketCap
+// Ticker information from CoinGecko
 bot.on(/^\/info$/i, async (msg) => {
   updateCalls(msg);
-  const text = 'MCRN';
+  const symbolID = "macaronswap";
   // Checks if the same argument has been passed into the command in the last 5 minutes
-  if (cache[text] && isValidCache(cache[text], cache[text].last_updated)) {
-    return msg.reply.text(formatInfo(cache[text]), { asReply: true });
+  if (
+    cache[symbolID] &&
+    isValidCache(cache[symbolID], cache[symbolID].last_updated)
+  ) {
+    return msg.reply.text(formatInfo(cache[symbolID]), { asReply: true });
   } else {
-    if (isNaN(text)) {
-      const symbol = text.toUpperCase();
-      cmcClient.getQuotes({ symbol: symbol }).then((info) => {
-        console.log(info);
-        if (!('data' in info && symbol in info.data))
-          return msg.reply.text(NO_CURRENCY, { asReply: true });
-        const data = info.data[symbol];
-        cache[symbol] = data;
-        return msg.reply.text(formatInfo(data), { asReply: true });
-      });
-    } else {
-      const rank = parseInt(text);
-      if (rank === 0) return msg.reply.text(RANK_NOT_IN_RANGE, { asReply: true });
-      cmcClient.getTickers({ start: rank, limit: 1 }).then((info) => {
-        console.log(info);
-        if (info.data.length === 0) return msg.reply.text(NO_CURRENCY_RANK, { asReply: true });
-        const data = info.data[0];
-        cache[text] = data;
-        return msg.reply.text(formatInfo(data), { asReply: true });
-      });
-    }
-  }
-});
+    CoinGeckoClient.coins.fetch(symbolID).then((info) => {
+      const data = info.data;
+      fetchStats().then((apiResp) => {
+        data.macaron_api_market_cap = apiResp.market_cap;
+        data.macaron_api_circulating_market_cap =
+          apiResp.circulating_market_cap;
+        data.macaron_api_total_supply = apiResp.total_supply;
+        data.macaron_api_circulating_supply = apiResp.circulating_supply;
 
-bot.on(/^\/priceCMC$/i, async (msg) => {
-  updateCalls(msg);
-  const text = 'MCRN';
-  // Checks if the same argument has been passed into the command in the last 5 minutes
-  if (cache[text] && isValidCache(cache[text], cache[text].last_updated)) {
-    return msg.reply.text(formatPrice(cache[text]), { asReply: true });
-  } else {
-    if (isNaN(text)) {
-      const symbol = text.toUpperCase();
-      cmcClient.getQuotes({ symbol: symbol }).then((info) => {
-        console.log(info);
-        if (!('data' in info && symbol in info.data))
-          return msg.reply.text(NO_CURRENCY, { asReply: true });
-        const data = info.data[symbol];
-        cache[symbol] = data;
-        return msg.reply.text(formatPrice(data), { asReply: true });
+        cache[symbolID] = data;
+
+        return msg.reply.text(formatInfo(data), { asReply: true });
       });
-    } else {
-      const rank = parseInt(text);
-      if (rank === 0) return msg.reply.text(RANK_NOT_IN_RANGE, { asReply: true });
-      cmcClient.getTickers({ start: rank, limit: 1 }).then((info) => {
-        console.log(info);
-        if (info.data.length === 0) return msg.reply.text(NO_CURRENCY_RANK, { asReply: true });
-        const data = info.data[0];
-        cache[text] = data;
-        return msg.reply.text(formatPrice(data), { asReply: true });
-      });
-    }
+    });
   }
 });
 
 bot.on(/^\/price$/i, async (msg) => {
   updateCalls(msg);
-  const text = '0xacb2d47827C9813AE26De80965845D80935afd0B'.toLowerCase();
+  const symbolID = "macaronswap";
   // Checks if the same argument has been passed into the command in the last 5 minutes
-  if (cache[text] && isValidCache(cache[text], cache[text].last_updated)) {
-    return msg.reply.text(formatPricePancake(cache[text]), { asReply: true });
+  if (
+    cache[symbolID] &&
+    isValidCache(cache[symbolID], cache[symbolID].last_updated)
+  ) {
+    return msg.reply.text(formatPrice(cache[symbolID]), {
+      asReply: true,
+    });
   } else {
-    var info = await fetchPancakeToken(text)
-      console.log(info);
-      if (info == null)
-        return msg.reply.text(NO_CURRENCY, { asReply: true });
-
-      cache[info.symbol] = info;
-      return msg.reply.text(formatPricePancake(info), { asReply: true });
+    var info = await fetchStats("https://www.macaronswap.finance/api/prices");
+    cache[symbolID] = info;
+    const address = "0xacb2d47827C9813AE26De80965845D80935afd0B".toLowerCase();
+    return msg.reply.text(formatPrice(info.data[address]), { asReply: true });
   }
 });
-
-// Total market information from CoinMarketCap
-/*
-bot.on('/global', async (msg) => {
-  updateCalls(msg);
-  // Checks if global command has been called in last 5 minutes
-  if (isValidCache(cache.global, cache.global.last_updated)) {
-    return msg.reply.text(formatGlobalInfo(cache.global), { asReply: true });
-  }
-  cmcClient.getGlobal().then((info) => {
-    console.log(info);
-    cache.global = info.data;
-    return msg.reply.text(formatGlobalInfo(info.data), { asReply: true });
-  });
-});
-*/
-
-// Latest exchange price from Binance
-/*
-bot.on(/^\/(.+)$/i, async (msg, props) => {
-  const text = props.match[1].toLowerCase();
-  if (isValidTickerText(props, text)) {
-    updateCalls(msg);
-    // Checks if command has been called in the past 5 minutes
-    if (isValidCache(cache.bin.data, cache.bin.last_updated)) {
-      return msg.reply.text(formatBinanceInfo(cache.bin.data, text.toUpperCase()), {
-        asReply: true,
-      });
-    } else {
-      binance.prices((_, ticker) => {
-        cache.bin.last_updated = new Date();
-        cache.bin.data = ticker;
-        console.log('Called Binance API');
-        return msg.reply.text(formatBinanceInfo(ticker, text.toUpperCase()), { asReply: true });
-      });
-    }
-  }
-});
-*/
 
 bot.start();
 
-function fetchPancakeToken(address){
-  let url = "https://www.macaronswap.finance/api/prices";
-
-  let options = {json: true};
+function fetchStats(url = "https://www.macaronswap.finance/api/stats") {
+  const options = { json: true };
 
   return new Promise((resolve, reject) => {
     request(url, options, (error, res, body) => {
-        if (error) reject(error);
-        if (res.statusCode != 200) {
-            reject('Invalid status code <' + res.statusCode + '>');
-        }
-
-        let json = JSON.parse(JSON.stringify(body))
-        resolve(json["data"][address])
+      if (error) reject(error);
+      if (res.statusCode != 200) {
+        reject("Invalid status code <" + res.statusCode + ">");
+      }
+      let json = JSON.parse(JSON.stringify(body));
+      resolve(json);
     });
   });
 }
 
-// Formats the output of the json for better readability
 function formatInfo(info) {
-  let output = info.name + ' (' + info.symbol + ')\n';
-  output += 'CoinMarketCap ID: ' + info.id + '\n';
-  output += 'CoinMarketCap Rank: ' + info.cmc_rank + '\n';
-  output += 'https://coinmarketcap.com/currencies/' + info.slug + '/\n\n';
+  let output = info.name + " (" + info.symbol.toUpperCase() + ")\n";
+  output += "https://macaronswap.finance\n";
+  output += "https://macaronswap.com\n";
+  output += "https://coingecko.com/en/coins/" + info.id + "/\n";
+  output += "CoinGecko Rank: " + info.coingecko_rank + "\n\n";
 
-  const priceInfo = info.quote.USD;
-  output += 'Price USD: $' + formatNum(priceInfo.price) + '\n';
-  output += 'Market Cap: $' + formatNum(priceInfo.market_cap) + '\n';
-  output += '24h Volume: $' + formatNum(priceInfo.volume_24h) + '\n';
-  output += 'Available Supply: ' + formatNum(info.circulating_supply) + '\n';
-  output += 'Total Supply: ' + formatNum(info.total_supply) + '\n';
-  if (info.max_supply) {
-    output += 'Maximum Supply: ' + formatNum(info.max_supply) + '\n';
-  }
+  const priceInfo = info.market_data;
+  output += "Price USD: $" + formatNum(priceInfo.current_price.usd) + "\n";
+  output +=
+    "Market Cap: $" + formatNumAndTrunc(info.macaron_api_market_cap) + "\n";
+  output +=
+    "Circulating Market Cap: $" +
+    formatNumAndTrunc(info.macaron_api_circulating_market_cap) +
+    "\n";
+  output +=
+    "24h Volume: $" + formatNumAndTrunc(priceInfo.total_volume.usd) + "\n";
+  output +=
+    "Total Supply: " + formatNumAndTrunc(info.macaron_api_total_supply) + "\n";
+  output +=
+    "Available Supply: " +
+    formatNumAndTrunc(info.macaron_api_circulating_supply) +
+    "\n";
 
-  output += '\nChange 1h: ' + formatNum(priceInfo.percent_change_1h) + '%\n';
-  if ('percent_change_24h' in priceInfo) {
-    output += 'Change 24h: ' + formatNum(priceInfo.percent_change_24h) + '%\n';
-  }
-  if ('percent_change_7d' in priceInfo) {
-    output += 'Change 7d: ' + formatNum(priceInfo.percent_change_7d) + '%\n';
-  }
-  output += '\n';
+  output +=
+    "\nChange 24h: " +
+    formatNum(priceInfo.price_change_percentage_24h.toFixed(2)) +
+    "%\n";
+  output +=
+    "Change 7d: " +
+    formatNum(priceInfo.price_change_percentage_7d.toFixed(2)) +
+    "%\n";
+  output +=
+    "Change 30d: " +
+    formatNum(priceInfo.price_change_percentage_30d.toFixed(2)) +
+    "%\n";
+  output += "\n";
 
-  return output + 'Last Updated: ' + new Date(info.last_updated).toString();
+  return output + "Last Updated: " + new Date(info.last_updated).toString();
 }
 
-// Formats the output of the json for better readability
 function formatPrice(info) {
-  let output = info.name + ' (' + info.symbol + ')\n';
+  let output = info.name + " (" + info.symbol + ")\n";
 
-  const priceInfo = info.quote.USD;
-  output += 'Price USD: $' + formatNum(priceInfo.price) + '\n';
-
-  output += '\nChange 1h: ' + formatNum(priceInfo.percent_change_1h) + '%\n';
-  if ('percent_change_24h' in priceInfo) {
-    output += 'Change 24h: ' + formatNum(priceInfo.percent_change_24h) + '%\n';
-  }
-  if ('percent_change_7d' in priceInfo) {
-    output += 'Change 7d: ' + formatNum(priceInfo.percent_change_7d) + '%\n';
-  }
-  output += '\n';
-
-  return output + 'Last Updated: ' + new Date(info.last_updated).toString();
-}
-
-function formatPricePancake(info) {
-  let output = info.name + ' (' + info.symbol + ')\n';
-
-  output += 'Price USD: $' + formatNum(info.price) + '\n';
-  return output + 'Price BNB: ' + info.price_BNB.substring(0, 8) + '\n';
-}
-
-// Formats the output of the json for global CMC data
-function formatGlobalInfo(info) {
-  const marketInfo = info.quote.USD;
-  let output =
-    'Total Market Cap: $' + parseInt(marketInfo.total_market_cap).toLocaleString() + '\n';
-  output += 'Total 24h Volume: $' + parseInt(marketInfo.total_volume_24h).toLocaleString() + '\n';
-  output +=
-    'Altcoin Market Cap: $' + parseInt(marketInfo.altcoin_market_cap).toLocaleString() + '\n';
-  output +=
-    'Altcoin 24h Volume: $' + parseInt(marketInfo.altcoin_volume_24h).toLocaleString() + '\n';
-  output += 'Bitcoin Percentage of Market Cap: ' + info.btc_dominance + '%\n';
-  output += 'Ethereum Percentage of Market Cap: ' + info.eth_dominance + '%\n\n';
-
-  output += 'Number of Active Currencies: ' + info.active_cryptocurrencies.toLocaleString() + '\n';
-  output += 'Number of Total Currencies: ' + info.total_cryptocurrencies.toLocaleString() + '\n';
-  output += 'Number of Active Markets: ' + info.active_market_pairs.toLocaleString() + '\n';
-  output += 'Number of Active Exchanges: ' + info.active_exchanges.toLocaleString() + '\n';
-  output += 'Number of Total Exchanges: ' + info.total_exchanges.toLocaleString() + '\n\n';
-
-  output += 'https://coinmarketcap.com/charts/' + '\n\n';
-
-  return output + 'Last Updated: ' + new Date(info.last_updated).toString();
-}
-
-// Formats the output for Binance exchange price
-function formatBinanceInfo(ticker, text) {
-  // The keys in the ticker object are exchanges in format equivalent to VENETH
-  const tradingPairs = ['ETH', 'BTC', 'BNB'];
-  let output = '';
-  tradingPairs.forEach((item) => {
-    const exc = text + item;
-    // Checks if there exists an exchange in the ticker with each of the pairs
-    if (ticker[exc]) {
-      output += ticker[exc] + ' ' + exc.replace(text, text + '/') + ' ';
-      output += '($' + formatBin(ticker[exc], ticker[item + 'USDT']) + ')\n';
-    }
-  });
-  if (ticker[text + 'USDT']) {
-    output += formatNum(ticker[text + 'USDT']) + ' ' + text + '/USDT\n';
-  }
-  return output == '' ? NO_TICKER : output;
-}
-
-// Check if valid ticker given for /<ticker>
-function isValidTickerText(props, text) {
-  return (
-    !text.startsWith('global') &&
-    !text.startsWith('info') &&
-    !props.match[0].startsWith('/chart') &&
-    text.match(/^[a-zA-Z]+$/) &&
-    text.length < 5
-  );
+  output += "\nPrice USD: $" + formatNum(info.price) + "\n";
+  return output + "Price BNB: " + info.price_BNB.substring(0, 8) + "\n";
 }
 
 // Check if the cache is valid
@@ -287,9 +139,9 @@ function formatNum(str) {
   return parseFloat(str).toLocaleString();
 }
 
-// Formats the displayed estimated dollar price for Binance exchange values
-function formatBin(price1, price2) {
-  return (parseFloat(price1) * parseFloat(price2)).toLocaleString();
+// Formats number string
+function formatNumAndTrunc(str) {
+  return Math.trunc(parseFloat(str)).toLocaleString();
 }
 
 // Checks for more than 30 calls a minute and updates number of calls
@@ -304,6 +156,6 @@ let calls = 0;
 resetNumCalls();
 setInterval(resetNumCalls, 60000);
 function resetNumCalls() {
-  console.log('Resetting number of calls at ' + new Date().toString());
+  console.log("Resetting number of calls at " + new Date().toString());
   calls = 0;
 }
